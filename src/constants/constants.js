b796ecbb0887e724b7d390990a7049e4737004dd
MAussem <Aussem.Maxwell@gmail.com(home)>
2021-11-03 19:05:18 -0500

    added images to projects. still need to fix some styling but getting very close

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'BrownsColoursPlus',
     description: "My first paid project was the development and design of https://www.brownscoloursplus.com. I used Wix editor and wix code environment (HTML, CSS, JS) to create a landing page for the clients business. Increased web traffic by improving navigation, creating dynamic media sections and adding social media plugins. Did a small amount of SEO.",
-    image: '/images/1.png',
+    image: '/images/BCP.png',
     tags: ['Wix Online Editor'],
     source: 'https://www.wix.com/mystunningwebsites/domain-solutions?utm_source=affiliate&utm_medium=paid_referral&utm_campaign=af_50@www.vilaitideals.com/&experiment_id=cake_94199975^458',
     visit: 'https://www.brownscoloursplus.com/',
@@ -11,7 +11,7 @@ export const projects = [
   {
     title: 'Flooring Depot',
     description: "I used wix editor and used wix code environment (HTML, CSS, JS) to create a landing page for the clients business. Mastered web designs from conception to completion and handled graphic design, artwork and digital application. Met with customer to determine and define goals surrounding graphics, design approaches and architecture. Small amount of SEO.",
-    image: '/images/2.png',
+    image: '/images/fd.png',
     tags: ['Wix Online Editor'],
     source: 'https://www.wix.com/mystunningwebsites/domain-solutions?utm_source=affiliate&utm_medium=paid_referral&utm_campaign=af_50@www.vilaitideals.com/&experiment_id=cake_94199975^458',
     visit: 'https://www.flooringdepot.org/',
@@ -20,7 +20,7 @@ export const projects = [
   {
     title: 'Designs By Angelika',
     description: "This is a portfolio i designed and developed for an interior designer. I met with customer to determine and define goals surrounding graphics, design approaches and architecture. Delivered innovated technical support to keep clients abreast of industry changes. Produced statistical reports for customer. Did some SEO as well",
-    image: '/images/3.jpg',
+    image: '/images/Dba.png',
     tags: ['HTML, CSS, JS'],
     source: 'https://github.com/MAussem/Angelika-HTML-CSS-JAVA',
     visit: 'https://designsbyangelika.org/',
@@ -29,7 +29,7 @@ export const projects = [
   {
     title: 'Auto Inspector Tool',
     description: "This is a web application that was built to inspect customers cars that come in for repair. Prepared a POA to successfully create a seamless experience for the users. Coded, tested and developed solutions in alignment with project requirements. Worked closely with business partners to create and deliver high-value solutions to meet their needs.",
-    image: '/images/4.jpg',
+    image: '/images/A0001-Logo.png',
     tags: ['React', 'Firebase'],
     source: 'https://github.com/MAussem/inspection-react',
     visit: 'https://google.com',
@@ -38,7 +38,7 @@ export const projects = [
   {
     title: 'Armies of Power LeaderBoard',
     description: "This is a Leaderboard for the Armies of Power NFT game on the WAX blockchain. We used AWS and a Demux to achieve a live leaderboard. Maintained updated knowledge of emerging blockchain technologies and vendor products. Improved operations by working with team members and customers to find workable solutions.",
-    image: '/images/4.jpg',
+    image: '/images/AOP.png',
     tags: ['React, Node, AWS'],
     source: 'https://github.com/MAussem/aop-leaderboard',
     visit: 'https://aopleaderboards.io/',
@@ -46,8 +46,8 @@ export const projects = [
   },
   {
     title: 'Armies of Power RYPWars',
-    description: "This is the second part of the Armies of Power project. I created and issued an eosio toekn for the customer and players. Then created a map that can be used to stake assets and view owned bases.",
-    image: '/images/4.jpg',
+    description: "This is the second part of the Armies of Power project. I created and issued an eosio token for the customer and players. Then created a map that can be used to stake assets and view owned bases.",
+    image: '/images/RYP.png',
     tags: ['React, Eosio, Cleos/Nodeos, C++ Smart Contracts'],
     source: 'https://github.com/MAussem/RYPwars',
     visit: 'https://rypwars.io/',
